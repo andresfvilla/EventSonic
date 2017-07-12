19290eb851d5cdaa3fae8fd5fa20fa8f09a79ca0
andresfvilla <andresfvilla88@gmail.com>
2017-07-12 13:35:03 -0400

    Update AppDelegate.m

--- EventSonic/EventSonic/AppDelegate.m
+++ EventSonic/EventSonic/AppDelegate.m
@@ -18,7 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    [GMSServices provideAPIKey:@"AIzaSyCptcXEIK3QAxhyvYGPYUWuUoyvdZEb9kw"];
+    [GMSServices provideAPIKey:@<insertAppKey>];
     
     NSError* configureError;
     [[GGLContext sharedInstance] configureWithError: &configureError];
